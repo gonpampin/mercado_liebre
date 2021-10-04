99f4fed0eb1c159991a7fafed1773fcc9e4e76c8
gonpampin <gonpampin@gmail.com>
2021-10-04 18:56:52 -0300

    la cuarta

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
-})
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
 
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
@@ -17,6 +17,4 @@ app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
 
-app.listen(process.env.Port || 3000 , () => {
-    console.log('Estamos corriendo en el puerto 3000');
-})
+app.listen( process.env.PORT || 3000  , () => console.log('Server corriendo en el puerto 3000'))
